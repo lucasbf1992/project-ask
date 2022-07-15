8122cd6e924843386782c34235646bada47fec65
lucasbf1992 <lucas.bf@outlook.com>
2022-07-15 16:02:35 -0300

    instalando o parseBody

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
+const bodyPaser = require('body-parser')
 
 //Estou dizendo para o Express usar EJS como View Engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(bodyPaser.urlencoded({extended: false}));
+app.use(bodyPaser.json);
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -12,6 +16,12 @@ app.get('/', (req, res) => {
 
 app.get('/ask', (req, res) => {
     res.render('ask')
+});
+
+app.post('/storeAsk', (req, res) => {
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send("FormularioRecebido")
 });
 
 app.listen(8080, () => {
